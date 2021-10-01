62f482ab4c5360418fecd16da518cb8c4d356b61
e87piez9jqf3bzksrob3 <e87piez9jqf3bzksrob3@gmail.com>
2021-10-01 00:23:56 -0400

    feat: set up class stencils with getters and correct variable names for json conversion

--- src/main/java/edu/brown/cs/student/apiClient/ApiClient.java
+++ src/main/java/edu/brown/cs/student/apiClient/ApiClient.java
@@ -1,9 +1,30 @@
 package edu.brown.cs.student.apiClient;
 
+import com.google.gson.Gson;
+
 public class ApiClient {
 
   private final String apiAuth;
   public ApiClient() {
     this.apiAuth = ClientAuth.getApiAuth();
   }
+
+  public String usersApiCall() {
+    String userJson = ApiSimulator.getSimulatedUsers();
+
+    System.out.println(userJson);
+
+    return userJson;
+  }
+
+  public String reviewsApiCall() {
+    String reviewJson = ApiSimulator.getSimulatedReviews();
+    return reviewJson;
+  }
+
+  public String rentsApiCall() {
+    String rentsJson = ApiSimulator.getSimulatedRents();
+    return rentsJson;
+  }
+
 }
--- src/main/java/edu/brown/cs/student/runway/Rent.java
+++ src/main/java/edu/brown/cs/student/runway/Rent.java
@@ -1,15 +1,59 @@
 package edu.brown.cs.student.runway;
 
 public class Rent {
-  private final String fit;
-  private final int userId; //long? string?
-  private final int itemId; // long? string?
-  private final int rating;
-  private final String rentedFor;
-  private final String category;
-  private final int size;
-  private final int id;
+  private String fit;
+  private int user_id; //long? string? or instance of User?
+  // private final User linkedUser;
+  private int item_id; // long? string?
+  private int rating;
+  private String rented_for;
+  private String category;
+  private int size;
+  private int id; // or just instance of Review?
+  // private final Review linkedReview;
 
-  public Rent() {
+//  public Rent(String fit, int userId, int itemId, int rating, String rentedFor,
+//              String category, int size, int id) {
+//    this.fit = fit;
+//    this.userId = userId;
+//    this.itemId = itemId;
+//    this.rating = rating;
+//    this.rentedFor = rentedFor;
+//    this.category = category;
+//    this.size = size;
+//    this.id = id;
+//    System.out.println(id);
+//  }
+
+  public String getRentedFor(){
+    return this.rented_for;
+  }
+
+  public String getFit() {
+    return fit;
+  }
+
+  public int getUser_id() {
+    return user_id;
+  }
+
+  public int getItem_id() {
+    return item_id;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public int getId() {
+    return id;
   }
 }
