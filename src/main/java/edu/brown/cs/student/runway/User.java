40a205d85d63a3df411e899c0683dfbd9a13770c
e87piez9jqf3bzksrob3 <e87piez9jqf3bzksrob3@gmail.com>
2021-10-01 00:23:56 -0400

    feat: set up class stencils with getters and correct variable names for json conversion

--- src/main/java/edu/brown/cs/student/runway/User.java
+++ src/main/java/edu/brown/cs/student/runway/User.java
@@ -2,14 +2,39 @@ package edu.brown.cs.student.runway;
 
 public class User {
 
-  private final int userId;
-  private final int weight; // String?
-  private final String bustSize;
-  private final String height;
-  private final int age;
-  private final String bodyType;
-  private final String horoscope;
+  private int user_id;
+  private String weight; // String?
+  private String bust_size;
+  private String height;
+  private int age;
+  private String body_type;
+  private String horoscope;
 
-  public User() {
+  public String getWeight() {
+    return this.weight;
+  }
+
+  public int getUserId() {
+    return user_id;
+  }
+
+  public String getBustSize() {
+    return bust_size;
+  }
+
+  public String getHeight() {
+    return height;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getBodyType() {
+    return body_type;
+  }
+
+  public String getHoroscope() {
+    return horoscope;
   }
 }
