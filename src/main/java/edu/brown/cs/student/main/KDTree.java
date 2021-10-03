0097dee4e87008cfc53ecc59e4e642879b78ea31
lznuscrfbj123shine4we5 <90661309+lznuscrfbj123shine4we5@users.noreply.github.com>
2021-10-03 01:27:21 -0400

    Ready for testing, everything implemented

--- src/main/java/edu/brown/cs/student/main/KDTree.java
+++ src/main/java/edu/brown/cs/student/main/KDTree.java
@@ -1,116 +1,260 @@
 package edu.brown.cs.student.main;
 
+import edu.brown.cs.student.runway.Runway;
+
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 
 
 public class KDTree {
-  private Node root;
-  private ArrayList<List<Hashtable<String,String>>> sortedLists;
-  private String[] dims;
-/*
-TODO: Reformat code to use User/Runway classes from API component instead of Hashtable
- */
-  public KDTree(List<Hashtable<String,String>> dataList, String[] dimensions) {
+  private final Node root;
+  private final ArrayList<Runway[]> sortedLists;
+  private final String[] axes;
+  private final ArrayList<Integer> visited;
+
+  public KDTree(Runway[] dataArr, String[] dimensions) {
+    visited = new ArrayList<Integer>();
     root = new Node();
     sortedLists = new ArrayList<>();
+
+    //created versions of the data sorted by each axis
     for(String dimension : dimensions){
-      Collections.sort(dataList, (o1,o2) -> {
-        return (int) (getNumVal(o1, dimension) - getNumVal(o2, dimension));
+      Arrays.sort(dataArr, (o1, o2) -> {
+        try {
+          return (int) (getNumVal((Runway) o1, dimension) - getNumVal((Runway) o2, dimension));
+        } catch (Exception e) {
+          e.printStackTrace();
+          return 0;
+        }
       });
-      sortedLists.add(dataList);
+      sortedLists.add(dataArr);
     }
 
-    root.data = null;
-    dims = dimensions;
+    axes = dimensions;
+    root.data = sortedLists.get(0)[dataArr.length/2];
+    visited.add(root.data.getUserId());
     setupTree(0, root);
   }
 
-  public void setupTree(int index, Node n){
+  public void setupTree(int axis, Node n){
     /*
-     * 1. find median value for particular dimension in current data list
-     * 2. split list into sublists with higher or lower values than median
-     * 3. recur on children with the next dimension, they should cycle.
-     * 4. recur until data list size is 1
+     * create left/right children
+     * left.data = next smallest neighbor on dimension
+     * right.data = next largest neighbor on dimension
      */
 
-    if(n.data.size() > 1) {
-      String dimension = dims[index];
+    List<Runway> tempList = Arrays.asList(sortedLists.get(axis));
 
-      // 1. find median value for particular dimension in current data list
-
-      n.data.sort((o1, o2) -> {
-        return (int) (getNumVal(o1, dimension) - getNumVal(o2, dimension));
-      });
-
-      int size = n.data.size();
-      double median = getNumVal(n.data.get(size / 2), dimension);
-
-      // 2.  split list into sublists with higher or lower values than median
-
-      n.left = new Node(n);
-      n.right = new Node(n);
-      n.left.data = n.data.subList(0, size / 2);
-      n.right.data = n.data.subList((size + 2) / 2, size - 1);
-
-      // 3. recur on children with the next dimension, they should cycle.
-
-      if (index == dims.length - 1) {
-        index = 0;
-      } else {
-        index++;
+    if(needsChild(sortedLists.get(axis), tempList.indexOf(n.data), true)) {
+      Node r = new Node();
+      r.data = tempList.get(tempList.indexOf(n.data) + 1);
+      //if assigned user is visited, keep looking to the right until it is not
+      while (visited.contains(r.data.getUserId())) {
+        r.data = tempList.get(tempList.indexOf(r.data) + 1);
       }
+      n.right = r;
+      visited.add(n.right.data.getUserId());
+      if(visited.size() < tempList.size()){
+        int x = 1;
+        if (axis+x == axes.length) {
+          x = -axis;
+        }
+        setupTree(axis+x, n.right);
+      }
+    }
 
-      setupTree(index, n.left);
-      setupTree(index, n.right);
+    if(needsChild(sortedLists.get(axis), tempList.indexOf(n.data), false)) {
+      Node l = new Node();
+      l.data = tempList.get(tempList.indexOf(n.data) - 1);
+      while (visited.contains(l.data.getUserId())) {
+        l.data = tempList.get(tempList.indexOf(l.data) - 1);
+      }
+      n.left = l;
+      visited.add(n.left.data.getUserId());
+
+
+      if(visited.size() < tempList.size()){
+        int x = 1;
+        if (axis+x == axes.length) {
+          x = -axis;
+        }
+        setupTree(axis+x, n.left);
+      }
+    }
+
+  }
+
+  public boolean needsChild(Runway[] array, int i, boolean right){
+    int dir;
+    if(right){
+      dir = 1;
+    }else{dir = -1;}
+
+    while(true){
+      //if index is at the min/max, return false. No child needed.
+      if(i+dir == array.length || i+dir == -1){
+        return false;
+      }
+      /*
+      If the next user in line is visited, increase i and loop again.
+      Otherwise return true - suitable child exists.
+       */
+      if(visited.contains(array[i+dir].getUserId())){
+        i += dir;
+      } else{
+        return true;
+      }
     }
   }
 
   /*
   * This method returns an inputted user's height,weight, or age as a double.
    */
-  public double getNumVal(Hashtable<String,String> user, String dimension){
-    if(dimension.equals("height")){
-      //convert height string, written in feet and inches, into a double in inches.
-      //ex: " 6' 7" " --> 79
-
-      //first, remove inches marker (double quote character)
-      String s = user.get(dimension).replace("\"", "");
-      //split remaining string into feet value and inches value
-      String[] nums = s.split("'");
-
-      //Take resulting string array and transcribe it to an int array
-      //Note: There may be a better way to do this in Java, I'm more familiar with doing it in Python.
-      int[] height = new int[2];
-      for(int i = 0; i < 2; i++){
-        height[i] = Integer.parseInt(nums[i]);
-      }
-
-      //return height in inches
-      return (height[0]*12)+height[1];
-
-    } else{
-      return Integer.parseInt(user.get(dimension).replaceAll("[a-zA-Z]",""));
+  public int getNumVal(Runway user, String axis) throws Exception {
+    switch(axis){
+      case("weight"):
+        return user.getWeight();
+      case("height"):
+        return user.getHeight();
+      case("age"):
+        return user.getAge();
+      default:
+        throw new Exception("Unexpected axis input.");
     }
   }
 
-  public List<Hashtable<String,String>> knn(int k, String userID){
+  public Runway[] knn(int k, int weight, int height, int age){
+    ArrayList<Runway> neighbors = new ArrayList<>();
+
     /*
-
+    Array to keep track of the index and distance of the furthest
+    neighbor from target. Note: furthest[0] is the index within
+    neighbors, furthest[1] is the distance from target point.
      */
+    int[] furthest = new int[2];
 
+    return knnHelper(k,new int[]{weight,height,age}, root, neighbors, furthest, 0);
+  }
+
+  /*
+  Recursive helper method for knn(). Performs the K-Nearest Neighbors algorithm.
+  Takes in an int[] target which contains the weight/height/age from knn().
+  Also requires an axis number and an array of values for the furthest neighbor,
+  explained in the comments for knn().
+   */
+  private Runway[] knnHelper(int k, int[] target, Node curr,
+                             List<Runway> neighbors, int[] furthest, int axis){
+    //Get the straight-line (Euclidean) distance from your target point to the current node.
+    int eDist = euclideanDistance(target[0],target[1],target[2], curr.data);
+
+    /*
+    If the current node is closer to your target point than
+    one of your k-nearest neighbors, or if your collection
+    of neighbors is not full, update the list accordingly
+     */
+    if(eDist < furthest[1] || neighbors.size() < k){
+      neighbors.add(curr.data);
+      if(neighbors.size() > k){
+        //if there are now more than k neighbors saved, remove the furthest neighbor.
+        neighbors.remove(furthest[0]);
+        furthest = findFurthest(target,neighbors);
+      } else if(eDist > furthest[1]){
+        furthest[1] = eDist;
+        furthest[0] = neighbors.indexOf(curr.data);
+      }
+    }
+
+    //If there are no children on which to recur, return current list of neighbors as array.
+    if(curr.right == null && curr.left == null){
+      return neighbors.toArray(new Runway[0]);
+    }
+
+    /*
+    If the Euclidean distance between the target point and
+    the furthest neighbor is greater than the relevant axis distance
+    between the current node and target point, recur on
+    both children. Also recur on both children if you have found fewer
+    than k neighbors so far.
+     */
+    int axisDist = 0;
+    switch(axis){
+      case 0:
+        axisDist = target[axis] - curr.data.getWeight();
+        break;
+      case 1:
+        axisDist = target[axis] - curr.data.getHeight();
+        break;
+      case 2:
+        axisDist = target[axis] - curr.data.getAge();
+        break;
+    }
+    if(furthest[1] > Math.abs(axisDist) || neighbors.size() < k){
+      if(curr.left != null){
+        if(axis == axes.length-1){axis = 0;} else {axis += 1;}
+        Runway[] result = knnHelper(k,target,curr.left,neighbors,furthest,axis);
+        if(curr.right != null && result != null){
+          return knnHelper(k,target,curr.left,Arrays.asList(result),furthest,axis);
+        }
+      } else{
+        if(axis == axes.length-1){axis = 0;} else {axis += 1;}
+        return knnHelper(k,target,curr.right,neighbors,furthest,axis);
+      }
+    }
+
+    /*
+    If the previous if-statement is false, and you do not need
+    to recur down both children, then:
+
+    If the current node's coordinate on the relevant axis is
+    less than the target's coordinate on the relevant axis,
+    recur on the right child.
+
+    Else if the current node's coordinate on the relevant axis
+    is greater than the target's coordinate on the relevant axis,
+    recur on the left child.
+     */
+    if(axisDist > 0 && curr.left != null){
+      if(axis == axes.length-1){axis = 0;} else {axis += 1;}
+      return knnHelper(k,target,curr.left,neighbors,furthest,axis);
+    }
+    else if(axisDist <= 0 && curr.right != null){
+      if(axis == axes.length-1){axis = 0;} else {axis += 1;}
+      return knnHelper(k,target,curr.right,neighbors,furthest,axis);
+    }
     return null;
   }
 
-  public List<Hashtable<String,String>> knn(int k, int weight, int height, int age){
-    return null;
+  /*
+  Helper method to find the furthest neighbor and return its index and distance as an array.
+   */
+  private int[] findFurthest(int[] target, List<Runway> neighbors){
+    int index = 0;
+    int dist = 0;
+    for(Runway n : neighbors){
+      int nDist = euclideanDistance(target[0],target[1],target[2],n);
+      dist = Math.max(dist, nDist);
+      if(dist == nDist){
+        index = neighbors.indexOf(n);
+      }
+    }
+    return new int[]{index,dist};
   }
 
-  public class Node {
-    private Hashtable<String,String> data;
+  /*
+  A helper function that gets the straight line distance between a target point and
+  a given user (in terms of weight height and age).
+   */
+  private int euclideanDistance(int x, int y, int z, Runway r){
+    return (int) Math.abs(Math.sqrt(Math.pow(x-r.getWeight(), 2)+
+        Math.pow(y-r.getHeight(), 2)+Math.pow(z-r.getAge(), 2)));
+  }
+
+  private static class Node {
+    private Runway data;
     private Node left;
     private Node right;
   }
